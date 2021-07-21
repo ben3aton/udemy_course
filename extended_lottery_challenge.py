972e661571a668ab00ac926385b5f25bff8d4cb9
Ben Eaton <ben23301@hotmail.com>
2021-07-21 21:04:59 +0100

    First commit to udemy_course git repo

---
+++ extended_lottery_challenge.py
@@ -0,0 +1,17 @@
+"""Module to handle the extended lottery challenge"""
+
+import random
+
+# This line creates a set with 6 random numbers
+lottery_numbers = set(random.sample(range(22), 6))
+
+# Here are your players; find out who has the most numbers matching lottery_numbers!
+players = [
+    {'name': 'Rolf', 'numbers': {1, 3, 5, 7, 9, 11}},
+    {'name': 'Charlie', 'numbers': {2, 7, 9, 22, 10, 5}},
+    {'name': 'Anna', 'numbers': {13, 14, 15, 16, 17, 18}},
+    {'name': 'Jen', 'numbers': {19, 20, 12, 7, 3, 5}}
+]
+
+print('Test')
+print(players[0]['name'])
